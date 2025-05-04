5b3b9022631dc13265814b51e8fcd15bd9fba654
hz4110 <han8866792@gmail.com>
2025-05-04 15:24:07 +0800

    platform movement

--- Characters/platform.gd
+++ Characters/platform.gd
@@ -10,8 +10,7 @@ func _physics_process(delta):
         # 获取输入
         var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
         velocity = input_vector * speed
-        # 移动平台
-        move_and_slide(velocity)
+        position+=velocity*delta
 
 # 显示平台
 func show_platform():
